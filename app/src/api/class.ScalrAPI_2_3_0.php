d2cc32b7def37fb5c98031a4977017dcf09e6a1c
Alex Wheeler <wheelear@gmail.com>
2014-11-30 13:11:21 -0500

    API call FarmAddRole now supports CloudStack

--- app/src/api/class.ScalrAPI_2_3_0.php
+++ app/src/api/class.ScalrAPI_2_3_0.php
@@ -211,6 +211,10 @@ class ScalrAPI_2_3_0 extends ScalrAPI_2_2_0
             'chef.role_name',
             'chef.attributes',
 
+            'cloudstack.service_offering_id',
+            'cloudstack.network_id',
+            'cloudstack.network_type',
+
             'aws.availability_zone',
             'aws.instance_type',
             'aws.security_groups.list',
@@ -280,8 +284,8 @@ class ScalrAPI_2_3_0 extends ScalrAPI_2_2_0
         if (preg_match("/^[^A-Za-z0-9_-]+$/", $Alias))
             throw new Exception("Role Alias should has only 'A-Za-z0-9-_' characters");
 
-        if (!PlatformFactory::isOpenstack($Platform) && $Platform != SERVER_PLATFORMS::EC2)
-            throw new Exception("Only EC2 and Openstack roles are supported by this method");
+        if (!PlatformFactory::isOpenstack($Platform) && $Platform != SERVER_PLATFORMS::EC2 && $Platform != SERVER_PLATFORMS::CLOUDSTACK)
+            throw new Exception("Only EC2, Openstack, and CloudStack roles are supported by this method");
 
         if (!$this->Environment->isPlatformEnabled($Platform))
             throw new Exception("'{$Platform}' cloud is not configured in your environment");
